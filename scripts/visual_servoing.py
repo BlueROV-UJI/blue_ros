e4d27eeffabe8a1980fdc9806bd978871f4a25c1
Ahmed Harbi <60305754+ahmedharbii@users.noreply.github.com>
2022-12-28 14:41:30 +0100

    visual servoing functional

--- scripts/visual_servoing.py
+++ scripts/visual_servoing.py
@@ -4,6 +4,8 @@ import rospy
 import cv2
 import cv2.aruco as aruco
 import numpy as np
+import scipy
+import tf
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CompressedImage
 from geometry_msgs.msg import Twist, Vector3
@@ -14,10 +16,21 @@ class ArucoServoing:
         self.aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
         self.aruco_params = aruco.DetectorParameters_create()
         self.bridge = CvBridge()
-        self.kp = 0.1
+        self.kp = 0.0001
+        self.error_x_prev = 0
+        self.error_y_prev = 0
         self.compressed_flag = True
+
+        #Camera parameters
+        self.camera_matrix = np.array([],[],[])
+        self.dist_coeffs = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+
+
+
         rospy.Subscriber("/blue_rov1/CompressedImage", CompressedImage, self.image_callback)
         self.cmd_vel_publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        # self.goal_points = np.array([[509.5, 257.5], [107.5, 257.0]])
+        self.goal_points = np.array([[509.5, 257.5]])
 
     def detect_marker(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -27,12 +40,17 @@ class ArucoServoing:
         print("corners = {}".format(corners_np))
         print("corners.shape = {}".format(corners_np.shape))
         frame_markers = aruco.drawDetectedMarkers(image, corners, ids)
+        rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 0.1, self.camera_matrix, self.dist_coeffs)
+        transform = cv2.aruco.drawAxis(image, self.camera_matrix, self.dist_coeffs, rvec, tvec, 0.1)
+        self.transformed_position = self.transform_position(tvec, transform)
+        self.quaternion = self.cv2.rodrigues(rvec)
+
         cv2.imshow("image", image)
         cv2.waitKey(3)
         cv2.imshow("frame_markers", frame_markers)
         cv2.waitKey(3)
 
-
+        aruco_centers = []
         for corner in corners:
             mid = [0, 0]
             print("corner = {}".format(corner))
@@ -41,10 +59,11 @@ class ArucoServoing:
                 mid[0] = (point[0][0] + point[2][0]) / 2
                 mid[1] = (point[0][1] + point[2][1]) / 2
             print("mid = {}".format(mid))
+            aruco_centers.append(mid)
             cv2.circle(frame_markers, (int(mid[0]), int(mid[1])), 5, (0, 0, 255), -1) #-1 means filled circle
         cv2.imshow("frame_markers", frame_markers)
         cv2.waitKey(3)
-        return frame_markers
+        return frame_markers, aruco_centers
 
     def image_callback(self, image_msg):
         if self.compressed_flag:
@@ -61,23 +80,58 @@ class ArucoServoing:
         
         self.image_height = cv_image.shape[0]
         self.image_width = cv_image.shape[1]
+        frame_markers, aruco_centers = self.detect_marker(cv_image)
+        #Check if aruco is detected:
+        if len(aruco_centers) == 0:
+            self.cmd_vel_publisher.publish(Twist(Vector3(0, 0, 0), Vector3(0, 0, 0)))
+            return
+        else:
 
-        self.point_coordinates = (self.image_width/2, self.image_height/2)
-        self.aruco_coordinates = self.detect_marker(cv_image)
-        print('a7a')
-        # error_x = self.point_coordinates[0] - self.aruco_coordinates[0]
-        # error_y = self.point_coordinates[1] - self.aruco_coordinates[1]
-        # vel_x = self.kp * error_x
-        # vel_y = self.kp * error_y
-        # print("vel_x = {}".format(vel_x.shape))
-        # print("vel_y = {}".format(vel_y))
+            error_x = self.goal_points[0][0] - aruco_centers[0][0]
+            error_y = self.goal_points[0][1] - aruco_centers[0][1]
+            print("error_x = {}".format(error_x))
+            print("error_y = {}".format(error_y))
 
-        # self.cmd_vel_publisher.publish(Twist(Vector3(float(vel_x[0]), 0, 0), Vector3(0, 0, float(vel_y[0]))))
+            vel_x = self.kp * (error_x + self.error_x_prev)
+            vel_y = self.kp * (error_y + self.error_y_prev)
+            self.error_x_prev = vel_x
+            self.error_y_prev = vel_y
+            # vel_y = 0
+            # vel_z = self.kp * error_y
+            vel_z = 0
+            print("vel_x = {}".format(vel_x))
+            print("vel_y = {}".format(vel_y))
+            print("vel_z = {}".format(vel_z))
 
-        #########################################
-        # print("vel_x = {}".format(vel_x))
-        # print("vel_y = {}".format(vel_y))
+            self.cmd_vel_publisher.publish(Twist(Vector3(float(-vel_z), float(vel_x), float(vel_y)), Vector3(0, 0, 0)))
 
+
+
+
+    def transform_position(self, position, transform):
+    # Create a quaternion from the rotation part of the transform matrix
+        quat = tf.transformations.quaternion_from_matrix(transform)
+        
+        # Create a transform object from the position and quaternion
+        transform = tf.Transform(translation=position, rotation=quat)
+        
+        # Use the transform object to transform the position to the new frame
+        transformed_position = transform * position
+        
+        return transformed_position
+    # def compute_kp(self, error):
+    #     #System Dynamics
+    #     A = np.array([[0, 1], [0, 0]])
+    #     B = np.array([[0], [1]])
+    #     C = np.array([[1, 0]])
+    #     D = np.array([[0]])
+    #     #LQR
+    #     Q = np.array([[1, 0], [0, 1]])
+    #     R = np.array([[1]])
+    #     K, _, _ = scipy.linalg.lqr(A, B, Q, R)
+    #     print("K = {}".format(K))
+
+    #     return K
 
 if __name__ == "__main__":
     rospy.init_node("aruco_servoing")
